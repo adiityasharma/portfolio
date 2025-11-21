f4378b8b05e6cd99991843c7721e0ca0a025e0cd
Aditya Sharma <adityasharma626367@gmail.com>
2025-11-21 15:07:14 +0530

    feat: implement Project section with animations, project cards, and data integration

---
+++ components/project-section.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { motion } from "framer-motion";
+import SectionHeading from "./section-heading";
+import { projectsData } from "@/lib/data";
+import { Project } from "../lib/types";
+import Link from "next/link";
+
+type ProjectCardProps = Project & {
+  index: number;
+};
+
+const fadeInAnimationVariants = {
+  initial: {
+    opacity: 0,
+    y: 100,
+  },
+  animate: (index: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: {
+      delay: 0.05 * index,
+    },
+  }),
+};
+
+const ProjectSection = () => {
+  const ProjectCard = (data: ProjectCardProps) => {
+    return (
+      <motion.div
+        variants={fadeInAnimationVariants}
+        initial="initial"
+        whileInView="animate"
+        viewport={{
+          once: true,
+        }}
+        custom={data.index}
+        className="flex flex-col rounded border p-4 cursor-pointer"
+      >
+        <Link
+          href={data.links.preview}
+          target="_blank"
+          aria-label={data.title}
+          className="overflow-hidden rounded"
+        >
+          <video
+            src={data?.video}
+            autoPlay
+            muted
+            loop
+            playsInline
+            className="pointer-events-none mx-auto object-cover h-40 w-full object-top"
+          />
+        </Link>
+        <h3 className="mt-4 text-xl font-medium ">{data.title}</h3>
+        <p className="text-muted-foreground mb-4 mt-1">{data.description}</p>
+        <div className="flex flex-wrap gap-2">
+          {data.technologies.map((tech) => (
+            <div key={tech} className="rounded-full border px-3 py-1 text-sm ">
+              {tech}
+            </div>
+          ))}
+        </div>
+      </motion.div>
+    );
+  };
+
+  return (
+    <section id="projects" className="my-10 scroll-mt-28 md:mb-20">
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.175 }}
+        viewport={{
+          once: true,
+        }}
+      >
+        <SectionHeading
+          heading="Projects"
+          content="Projects I worked on. Each of them containing it's own case study"
+        />
+      </motion.div>
+
+      <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
+        {projectsData.map((i, index) => (
+          <ProjectCard key={i.title} {...i} index={index} />
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default ProjectSection;
