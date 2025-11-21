837a93c3b11ee3800357f1f7338552b90dd8e65e
Aditya Sharma <adityasharma626367@gmail.com>
2025-11-21 12:49:41 +0530

    feat: add multiple sections to the homepage and implement grid pattern component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,24 @@
+import AboutSection from "@/components/about-section";
+import ContactSection from "@/components/contact-section";
+import Divider from "@/components/divider";
+import ExperienceSection from "@/components/experience-section";
+import Footer from "@/components/footer";
 import Header from "@/components/header";
+import HeroSection from "@/components/hero-section";
+import ProjectSection from "@/components/project-section";
 
 export default function Home() {
   return (
-    <div className="max-w-3xl mx-auto flex flex-col items-center min h-screen" >
-      <Header/>
+    <div className="max-w-3xl mx-auto flex flex-col items-center min h-screen">
+      <Header />
+      <HeroSection />
+      <AboutSection />
+      <Divider />
+      <ExperienceSection />
+      <Divider />
+      <ProjectSection />
+      <ContactSection />
+      <Footer />
     </div>
   );
 }
---
+++ components/hero-section.tsx
@@ -0,0 +1,115 @@
+"use client";
+import { GridPattern } from "./ui/grid-pattern";
+import { cn } from "@/lib/utils";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { siteConfig } from "@/lib/site-config";
+import { Button } from "./ui/button";
+import { Icons } from "./icons";
+
+const HeroSection = () => {
+  return (
+    <section className="relative my-10 mb-60 flex scroll-mt-96 flex-col items-center gap-5 text-center sm:mt-28">
+      <GridPattern
+        width={80}
+        height={120}
+        squares={[
+          [0, 3],
+          [3, 1],
+          [2, 0],
+          [7, 2],
+          [8, 3],
+          [2, 4],
+          [6, 4],
+        ]}
+        strokeDasharray={"4"}
+        className={cn(
+          "absolute inset-0",
+          "mask-[radial-gradient(400px_circle_at_center,white,transparent)]",
+          "inset-x-0 inset-y-[-30%] h-[200%] skew-y-12",
+          "opacity-70"
+        )}
+      />
+
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{
+          type: "tween",
+          duration: 0.2,
+        }}
+      >
+        <Link
+          href={siteConfig.links.contactForm}
+          className="flex items-center gap-3 rounded border px-3 py-1"
+        >
+          <span className="relative flex size-2">
+            <span className="absolute flex size-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+            <span className="relative flex size-2 rounded-full bg-green-400 "></span>
+          </span>
+          <span className="text-sm ">Available for work!</span>
+        </Link>
+      </motion.div>
+
+      <motion.h1
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        className="font-heading max-w-3xl text-4xl font-extrabold md:text-5xl"
+      >
+        I'm a{" "}
+        <span className="bg-linear-to-r from-rose-700 to-pink-600 bg-clip-text text-transparent ">
+          Creative Frontend
+        </span>{" "}
+        developer carfting modern digital experiences.
+      </motion.h1>
+
+      <motion.p
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+        className="text-muted-foreground max-w-xl"
+      >
+        You have Project in mind? Let's talk about it!
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.2 }}
+        className="flex flex-row gap-2"
+      >
+        <Button asChild size="lg">
+          <Link href={siteConfig.links.contactForm}>
+            Get in touch <Icons.arrowRight className="ml-2 size-4" />
+          </Link>
+        </Button>
+        <Button asChild variant="outline" size="lg" className="hidden sm:flex ">
+          <a href={siteConfig.links.cvPdf} download>
+            {" "}
+            Download CV <Icons.download className="ml-2 size-4" />{" "}
+          </a>
+        </Button>
+        <Button asChild variant="outline" size="icon">
+          <Link
+            href={siteConfig.links.linkedin}
+            aria-label="linkedin"
+            target="_blank"
+          >
+            <Icons.linkedin className="size-5" />
+          </Link>
+        </Button>
+        <Button asChild variant="outline" size="icon">
+          <Link
+            href={siteConfig.links.github}
+            aria-label="github"
+            target="_blank"
+          >
+            <Icons.github className="size-5" />
+          </Link>
+        </Button>
+      </motion.div>
+    </section>
+  );
+};
+
+export default HeroSection;
